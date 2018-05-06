bd5ca714f1eeb55a1b8ffb8b2c9b7a6c8ab7a2f4
Iryna Bakal <irynabakal@Irynas-MacBook-Pro.local>
2018-05-06 11:46:09 +0300

    made post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,8 +9,9 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
-    }
+        posts: [],
+        selectedPostId: null
+    };
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -25,10 +26,19 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts});
                 //console.log(response);
             });
-    }
+    };
+
+    postSelectedHandler = id => {
+        this.setState({selectedPostId: id});
+    };
+
     render () {
         const posts = this.state.posts.map( post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={this.postSelectedHandler.bind(this, post.id)} />
         });
 
         return (
@@ -37,7 +47,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
