919393e8ec2fae2875121f2919613b0caf7dc67d
Iryna Bakal <irynabakal@Irynas-MacBook-Pro.local>
2018-05-09 14:19:14 +0300

    fixed issue with overriding header in axios instance

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {'Authorization': 'AUTH TOKEN FROM INSTANCE'}
 });
-
-instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 
 // instance.interceptors.request.use(request => {
 //   console.log(request);
