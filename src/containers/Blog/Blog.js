5364982c956e9d05a82c482c19a204ee82ec476f
Iryna Bakal <iryna.bakal@wdc.com>
2018-05-15 23:58:20 +0300

    using Link to switch pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class Blog extends Component {
   render() {
@@ -10,8 +11,8 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href='/'>Home</a></li>
-              <li><a href='/new-post'>New Post</a></li>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/new-post'>New Post</Link></li>
             </ul>
           </nav>
         </header>
@@ -22,6 +23,7 @@ class Blog extends Component {
           <Route path='/' render={ () => <h1>home 2</h1> } />
         */}
         <Route path='/' exact component={Posts} />
+        <Route path='/new-post' component={NewPost} />
       </div>
     );
   }
