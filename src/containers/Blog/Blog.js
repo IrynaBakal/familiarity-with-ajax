2bfb741fb412d5170c5633819d545001985e8aaa
Iryna Bakal <iryna.bakal@wdc.com>
2018-05-15 23:37:31 +0300

    setting up & rendering routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
   render() {
@@ -14,7 +15,10 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        {/* if path match, route will be replace itself with content you define in render */}
+        <Route path='/' exact render={ () => <h1>home</h1> } />
+        {/* second route will be rendered always */}
+        <Route path='/' render={ () => <h1>home 2</h1> } />
       </div>
     );
   }
